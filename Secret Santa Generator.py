c357582197a0ed70e769382d8b9b70b19574f291
elisa-maguina <elisa.maguina@gmail.com>
2019-12-10 22:27:38 -0500

    Add files via upload
    This is a random generator that does not use pop() (so it doesn't remove previously used list items). Will be fixed.

---
+++ Secret Santa Generator.py
@@ -0,0 +1,21 @@
+import random
+import emoji    
+  
+
+santa_recip_list = ['Melissa', 'Ivan', 'Lidia', 'Enrique', 'Matt', 'Elisa', 'Zenaida']
+random_santa_recip = random.choice(santa_recip_list)
+max_amount = '$30 (+/- $5)'
+rule_intro = ('The rules for this year are as follows:')
+rule_1 = ('\n1. Stay within the dollar limit of ' + max_amount + '.')
+rule_2 = ('\n2. Gift wrap, cards, and accessesories do NOT count towards the dollar limit.')
+Wishlist = "https://tinyurl.com/xmaslist19"
+Deadline = ("EOD 12/14/2019")
+Welcome_Message = "Ho ho ho! Welcome to Secret Santa 2019."
+print((emoji.emojize(':sparkles: ')), Welcome_Message, (emoji.emojize(':sparkles:')))
+print("You have been randomly selected to be", random_santa_recip + "'s", "Secret Santa!")
+print(rule_intro, rule_1, rule_2,)
+print("Please fill out the wishlist at " + Wishlist + " by " + Deadline + ".")
+print("Merry Christmas!")
+
+
+
